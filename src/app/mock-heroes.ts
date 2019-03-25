3f0f5e58dc8701b5ef16c0d004666a23dc7130fa
dedechandran <dedechandra852.dc@gmail.com>
2019-03-25 07:07:29 +0700

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+    {id : 1 , name : 'Ich'},
+    {id : 2 , name : 'Ni'},
+    {id : 3 , name : 'San'},
+    {id : 4 , name : 'Yon'},
+    {id : 5 , name : 'Go'},
+    {id : 6 , name : 'Roku'},
+    {id : 7 , name : 'Nana'}
+];
